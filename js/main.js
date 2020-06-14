d8827092aefb448f7140be0ef55b6d468a8066bd
John Conkle <green@johns-air.lan>
2020-06-14 14:34:30 -0700

    fixing lorem ipsum language

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const myWork = [
   {
     'title': "Click Here!",
     'href': "https://www.google.com/maps/place/29+Barnacle+Ln,+Cushing,+ME+04563/data=!4m2!3m1!1s0x4cadb8329ff27aab:0x1a2eda5dea4470b?sa=X&ved=2ahUKEwiw9pD9mYLqAhUMIjQIHWmDCxkQ8gEwAHoECAsQAQ",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': "After a long, productive, and rewarding career in the software industry, I dream of getting the heck out of the city, ideally to this vacation property on the coast of Maine.  It's an ocean-front family compound with both a cottage and a boathouse, so it can sleep upwards of thirty people in a pinch.  It has west-facing views so you see the sunset.  Even better, it's extremely rural and you cannot ask for better social distancing.",
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/myVacationProperty.jpeg",
